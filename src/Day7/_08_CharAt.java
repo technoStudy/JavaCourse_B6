56af88900df3e1349fac092730edd732f0cd2048
hmk10 <halitmk@gmail.com>
2022-05-05 20:16:34 -0400

    String Methods | CharAt

---
+++ src/Day7/_08_CharAt.java
@@ -0,0 +1,17 @@
+package Day7;
+
+public class _08_CharAt {
+
+    public static void main(String[] args) {
+
+        String message = "Hello World";
+
+        char letter = message.charAt(5);
+        System.out.println(letter);
+
+        System.out.println(message.charAt(0));
+        System.out.println(message.charAt(3));
+
+    }
+
+}
