d670d919a8670361e69fdcd150c2e48432bdf041
hmk10 <halitmk@gmail.com>
2022-05-06 19:50:49 -0400

    String Methods | Trim

---
+++ src/Day8/_15_Trim.java
@@ -0,0 +1,16 @@
+package Day8;
+
+public class _15_Trim {
+
+    public static void main(String[] args) {
+
+        String myString = "             Hello           World               ";
+
+        System.out.println(myString);
+
+        // trim method will remove spaces before and after your string
+        System.out.println(myString.trim());
+
+    }
+
+}
