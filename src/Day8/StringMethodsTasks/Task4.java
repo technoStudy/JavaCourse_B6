dee754cd4940a18d3384276208a0b0ccfb28b338
hmk10 <halitmk@gmail.com>
2022-05-06 21:57:27 -0400

    String Methods | Tasks

---
+++ src/Day8/StringMethodsTasks/Task4.java
@@ -0,0 +1,25 @@
+package Day8.StringMethodsTasks;
+
+public class Task4 {
+
+    //                  TASK
+    // Create th following Strings:
+        // String firstString = "123456789";
+        // String secondString = "0123456789";
+
+    // Part1: find out the character at 5th index of String "123456789"
+    // Part2: find out the character at 5th index of String "0123456789"
+
+
+    public static void main(String[] args) {
+
+         String firstString = "123456789";
+         String secondString = "0123456789";
+
+        System.out.println(firstString.charAt(5));
+        System.out.println(secondString.charAt(5));
+
+    }
+
+
+}
---
+++ src/Day8/StringMethodsTasks/Task5.java
@@ -0,0 +1,24 @@
+package Day8.StringMethodsTasks;
+
+public class Task5 {
+
+    //                  TASK
+    // Create the following String:
+        // String message = "Hello TechnoStudy";
+
+    // convert "Hello TechnoStudy!" to lowercase letters
+    // convert "Hello TechnoStudy!" to uppercase letters
+
+    public static void main(String[] args) {
+
+        String message = "Hello TechnoStudy!";
+
+        String lowerCaseMessage = message.toLowerCase();
+        System.out.println(lowerCaseMessage);
+
+        String upperCaseMessage = message.toUpperCase();
+        System.out.println(upperCaseMessage);
+
+    }
+
+}
