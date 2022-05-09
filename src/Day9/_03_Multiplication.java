217be815213e57baf09727d4fefdb24497041aed
hmk10 <halitmk@gmail.com>
2022-05-09 19:37:29 -0400

    Arithmetic Expressions | Multiplication

---
+++ src/Day9/_03_Multiplication.java
@@ -0,0 +1,24 @@
+package Day9;
+
+public class _03_Multiplication {
+
+    public static void main(String[] args) {
+
+        // Multiplication using only values
+        System.out.println(5 * 3);
+
+        // Multiplication using only variables
+        int x = 8;
+        int y = 9;
+
+        System.out.println(x * y);
+
+        int result = x * y; // 8 * 9 - 72 | int result = 72
+        System.out.println(result);
+
+        // Multiplication between variable and value
+        System.out.println(x * 2);
+
+    }
+
+}
