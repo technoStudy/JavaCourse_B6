5fa864eb35a64afbdea8ae7aef58f87917136682
hmk10 <halitmk@gmail.com>
2022-05-09 19:16:40 -0400

    Arithmetic Expressions | Addition

---
+++ src/Day9/_01_Addition.java
@@ -0,0 +1,24 @@
+package Day9;
+
+public class _01_Addition {
+
+    public static void main(String[] args) {
+
+        // addition using only values
+        System.out.println(5 + 4);
+
+        int x = 1;
+        int y = 2;
+
+        // addition using only variables
+        System.out.println(x + y);
+
+        int result = x + y;
+        System.out.println(result);
+
+        // addition using variable and value
+        System.out.println(x + 9);
+
+    }
+
+}
