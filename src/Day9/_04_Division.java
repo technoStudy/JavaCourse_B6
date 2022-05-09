6da404c04107496c9cec54ab5bc11842d1d53d80
hmk10 <halitmk@gmail.com>
2022-05-09 19:56:06 -0400

    Arithmetic Expressions | Division

---
+++ src/Day9/_04_Division.java
@@ -0,0 +1,29 @@
+package Day9;
+
+public class _04_Division {
+
+    public static void main(String[] args) {
+
+        // Division only using values
+        System.out.println(9 / 2);
+
+        // Division using only variables
+        int x = 45;
+        int y = 5;
+
+        System.out.println(x / y);
+
+        int result = x / 9;
+        System.out.println(result);
+
+        // System.out.println(15 / 0); - not possible, you cannot divide by 0
+
+        int a = 25;
+        int b = 4;
+
+        double result2 = (double) a / (double) b;
+        System.out.println(result2);
+
+    }
+
+}
