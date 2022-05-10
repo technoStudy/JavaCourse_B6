88be53cbc3f0931235d0d572847c85c50b3fedaf
hmk10 <halitmk@gmail.com>
2022-05-09 20:43:19 -0400

    Arithmetic Expressions | Decrement Operator

---
+++ src/Day9/_07_Decrement.java
@@ -0,0 +1,22 @@
+package Day9;
+
+public class _07_Decrement {
+
+    public static void main(String[] args) {
+
+        // decrement: decreases value by 1
+
+        int x = 5;
+        x--;
+        System.out.println(x);
+
+        int y = 10;
+        System.out.println(--y);
+
+        int c = 15;
+        // post-decrement, value is returned first and then it's decreased by 1
+        System.out.println(c--);
+
+    }
+
+}
