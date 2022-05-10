283266afbd161ef73550e540025fd9e27385bb2b
hmk10 <halitmk@gmail.com>
2022-05-09 20:16:44 -0400

    Arithmetic Expressions | Modulus

---
+++ src/Day9/_05_Modulus.java
@@ -0,0 +1,18 @@
+package Day9;
+
+public class _05_Modulus {
+
+    public static void main(String[] args) {
+
+        int x = 8; // dividend
+        int y = 3; // divisor
+
+        System.out.println(x % y); // remainder from division
+
+        System.out.println(10 % 3);
+
+        System.out.println(100 % 10);
+
+    }
+
+}
