ff05f2af93f7c41e7bbfb69a076750916440c900
hmk10 <halitmk@gmail.com>
2022-05-09 21:16:03 -0400

    Arithmetic Expressions | Assignment Operators

--- src/Day9/_08_AssignmentOperators.java
+++ src/Day9/_08_AssignmentOperators.java
@@ -27,6 +27,18 @@ public class _08_AssignmentOperators {
         l -= 2; // l = l - 2; | decrease value of l by 2
         System.out.println(l);
 
+        int a = 10;
+        a *= 5; // a = a * 5; | multiply value of a by 5
+        System.out.println(a);
+
+        int b = 90;
+        b /= 9; // b = b / 9; | divide value of b by 9
+        System.out.println(b);
+
+        int p = 99;
+        p %= 10; // p = remainder from division 99/10
+        System.out.println(p);
+
     }
 
 }
