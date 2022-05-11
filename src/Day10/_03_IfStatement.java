c74f69573b360b34caf9640d3deefab2de9b6384
hmk10 <halitmk@gmail.com>
2022-05-10 20:49:42 -0400

    If Statement

---
+++ src/Day10/_03_IfStatement.java
@@ -0,0 +1,23 @@
+package Day10;
+
+public class _03_IfStatement {
+
+    public static void main(String[] args) {
+
+        int a = 1;
+        int b = 1;
+        int c = 2;
+
+
+        if (a == b) {
+            System.out.println("A equal B");
+        }
+
+
+        if (b == c) {
+            System.out.println("B equal C");
+        }
+
+    }
+
+}
