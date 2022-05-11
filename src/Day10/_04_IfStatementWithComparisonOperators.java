5a27823eb4c41cc07b06064037a1f7768af7d478
hmk10 <halitmk@gmail.com>
2022-05-10 21:24:15 -0400

    If Statement with Comparison Operators

---
+++ src/Day10/_04_IfStatementWithComparisonOperators.java
@@ -0,0 +1,27 @@
+package Day10;
+
+public class _04_IfStatementWithComparisonOperators {
+
+    public static void main(String[] args) {
+
+        int numberOfStudents = 10;
+
+        if (numberOfStudents > 20) {
+            System.out.println("There are more than 20 students in the class!");
+        }
+
+        if (numberOfStudents > 10) {
+            System.out.println("There are more than 10 students in the class!");
+        }
+
+        if (numberOfStudents < 10) {
+            System.out.println("There are less than 10 students in the class!");
+        }
+
+        if (numberOfStudents == 10) {
+            System.out.println("There are 10 students in the class!");
+        }
+
+    }
+
+}
