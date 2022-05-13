2aa11d9d4f199b407874bf71a29f61255bd1b558
hmk10 <halitmk@gmail.com>
2022-05-12 21:12:15 -0400

    If Else Statement

---
+++ src/Day11/_01_IfElse.java
@@ -0,0 +1,17 @@
+package Day11;
+
+public class _01_IfElse {
+
+    public static void main(String[] args) {
+
+        int income = 60_000;
+
+        if (income > 100_000) {
+            System.out.println("Your income is good!");
+        } else {
+            System.out.println("Your income is fair!");
+        }
+
+    }
+
+}
