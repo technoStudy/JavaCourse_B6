dd97d8b500eb19b5ec3a0399e38fc27a043c6789
hmk10 <halitmk@gmail.com>
2022-05-14 10:43:37 -0400

    Simplifying If Else

---
+++ src/Day13/_02_SimplifyIfStatement.java
@@ -0,0 +1,23 @@
+package Day13;
+
+public class _02_SimplifyIfStatement {
+
+    public static void main(String[] args) {
+
+//        int income = 120_000;
+//        boolean hasHighIncome;
+//
+//        if (income > 100_000)
+//            hasHighIncome = true;
+//        else
+//            hasHighIncome = false;
+
+
+        int income = 120_000;
+        boolean hasHighIncome = income > 100_000;
+
+        System.out.println(hasHighIncome);
+
+    }
+
+}
