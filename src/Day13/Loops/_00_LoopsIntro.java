3bc884dd2825fb0414eb6cb1d8baf0f86f3809b4
hmk10 <halitmk@gmail.com>
2022-05-14 12:18:19 -0400

    Loops Intro

---
+++ src/Day13/Loops/_00_LoopsIntro.java
@@ -0,0 +1,23 @@
+package Day13.Loops;
+
+public class _00_LoopsIntro {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+
+        // We'll never write a code like this.
+        // Whenever we have repeated code we'll use LOOPS
+
+    }
+
+}
