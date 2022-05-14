b89c031a249b099e05b9052ca8d873621474b5ee
hmk10 <halitmk@gmail.com>
2022-05-14 12:32:11 -0400

    While Loop

---
+++ src/Day13/Loops/_02_WhileLoop.java
@@ -0,0 +1,16 @@
+package Day13.Loops;
+
+public class _02_WhileLoop {
+
+    public static void main(String[] args) {
+
+        int counter = 0;
+
+        while (counter < 10) {
+            System.out.println("Hello World!");
+            counter++;
+        }
+
+    }
+
+}
