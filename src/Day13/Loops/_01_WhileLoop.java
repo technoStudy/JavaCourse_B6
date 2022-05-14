750e881358bed9bdd12a1f808f442d97910f5a9d
hmk10 <halitmk@gmail.com>
2022-05-14 12:23:33 -0400

    While Loop

---
+++ src/Day13/Loops/_01_WhileLoop.java
@@ -0,0 +1,20 @@
+package Day13.Loops;
+
+public class _01_WhileLoop {
+
+    public static void main(String[] args) {
+
+        // SYNTAX OF WHILE LOOP
+
+//        boolean condition;
+//
+//        while (condition) {
+//            System.out.println("While condition is true, print!!!");
+//        }
+
+    }
+
+
+
+
+}
