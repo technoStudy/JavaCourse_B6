ebd4c199ed96fa8ce936eac6a2bea7e7beeb3ccc
hmk10 <halitmk@gmail.com>
2022-05-16 19:33:03 -0400

    While Loop | Task | Homework

---
+++ src/Day14/Task4_WhileLoop.java
@@ -0,0 +1,30 @@
+package Day14;
+
+public class Task4_WhileLoop {
+
+    /*
+
+                            TASK
+        Sum all the odd numbers from 1 to 100 (1 + 3 + 5 ....)
+        Use While Loop!
+
+     */
+
+
+    public static void main(String[] args) {
+
+        int counter = 1;
+        int total = 0; // 0 + 1 + 3 + 5 .......
+
+        while (counter < 100) {
+
+            total = total + counter;
+            counter += 2;
+
+        }
+
+        System.out.println("Total for all the odd number from 1 to 100: " + total);
+
+    }
+
+}
