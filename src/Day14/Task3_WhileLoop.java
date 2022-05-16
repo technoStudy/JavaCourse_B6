d3771f9d26baff14208b664ea4a4fbed8672948e
hmk10 <halitmk@gmail.com>
2022-05-16 19:23:43 -0400

    While Loop | Task | Homework

---
+++ src/Day14/Task3_WhileLoop.java
@@ -0,0 +1,24 @@
+package Day14;
+
+public class Task3_WhileLoop {
+
+    /*
+
+                            TASK
+        Print all the even numbers from 0 to 100(hundred is not included)
+        Use While Loop!
+
+     */
+
+    public static void main(String[] args) {
+
+        int counter = 0;
+
+        while (counter < 100) {
+            System.out.println(counter);
+            counter += 2;
+        }
+
+    }
+
+}
