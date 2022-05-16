c99fd974bedf127abeecf07313b2e0d4baec2675
hmk10 <halitmk@gmail.com>
2022-05-16 19:18:20 -0400

    While Loop | Task | Homework

---
+++ src/Day14/Task2_WhileLoop.java
@@ -0,0 +1,26 @@
+package Day14;
+
+public class Task2_WhileLoop {
+
+
+    /*
+
+                        TASK
+        Print all the odd numbers from 1 to 100
+        Use While Loop!
+
+     */
+
+    public static void main(String[] args) {
+
+        int counter = 1;
+
+        while (counter < 100) {
+            System.out.println(counter);
+            counter += 2; // counter = counter + 2
+        }
+
+    }
+
+
+}
