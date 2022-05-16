010488787a7f56ccafe364f13326f1804c026ed3
hmk10 <halitmk@gmail.com>
2022-05-16 19:10:40 -0400

    While Loop | Task | Homework

---
+++ src/Day14/Task1_WhileLoop.java
@@ -0,0 +1,25 @@
+package Day14;
+
+public class Task1_WhileLoop {
+
+    /*
+
+                        TASK
+        Print all the numbers from 0 to 100(hundred included)
+        Use While Loop!
+
+     */
+
+
+    public static void main(String[] args) {
+
+        int counter = 0;
+
+        while (counter <= 100) {
+            System.out.println(counter);
+            counter++;
+        }
+
+    }
+
+}
