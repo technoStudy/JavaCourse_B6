8f3bfe8869b82540bacbf5152728d387ed78ec2d
hmk10 <halitmk@gmail.com>
2022-05-16 21:30:14 -0400

    For Loop

---
+++ src/Day14/_04_ForLoop.java
@@ -0,0 +1,23 @@
+package Day14;
+
+public class _04_ForLoop {
+
+    public static void main(String[] args) {
+
+        for (int counter = 0; counter < 5; counter++) {
+            System.out.println("Hello World!");
+        }
+
+
+        for (int counter = 1; counter <= 5; counter++) {
+            System.out.println("Hello World!");
+        }
+
+
+        for (int counter = 5; counter > 0; counter--) {
+            System.out.println("Hello World!");
+        }
+
+    }
+
+}
