800dc609a64217cad4066d3a6cf5974ca80af52a
hmk10 <halitmk@gmail.com>
2022-05-16 20:09:49 -0400

    Do While Loop

---
+++ src/Day14/_02_DoWhileLoop.java
@@ -0,0 +1,16 @@
+package Day14;
+
+public class _02_DoWhileLoop {
+
+    public static void main(String[] args) {
+
+        int counter = 0;
+
+        do {
+            System.out.println("Hello");
+            counter++;
+        } while (counter < 5);
+
+    }
+
+}
