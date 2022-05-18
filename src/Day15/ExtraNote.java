fdf57b517b85666412bd5342adad55dc3e3b41e1
hmk10 <halitmk@gmail.com>
2022-05-17 21:18:27 -0400

    Note about curly braces

---
+++ src/Day15/ExtraNote.java
@@ -0,0 +1,18 @@
+package Day15;
+
+public class ExtraNote {
+
+    public static void main(String[] args) {
+
+        // if you have only 1 line inside if you don't have to type {}
+        if (5 < 10)
+            System.out.println("5 less than 10");
+
+
+        // if you have only 1 line inside loop you don't have to type {}
+        for (int i = 0; i < 10; i++)
+            System.out.println(i);
+
+    }
+
+}
