82e62bc25a2d8221afba7384bb5c1b888be2e767
hmk10 <halitmk@gmail.com>
2022-05-17 20:18:31 -0400

    Infinite Loop

---
+++ src/Day15/_01_InfiniteLoop.java
@@ -0,0 +1,20 @@
+package Day15;
+
+public class _01_InfiniteLoop {
+
+    public static void main(String[] args) {
+
+        // Infinite Loop (with For)
+        for (int i = 0; i < 1;) {
+            System.out.println("Hello World!");
+        }
+
+
+        // Infinite Loop (with While)
+        while (true) {
+            System.out.println("TechnoStudy");
+        }
+
+    }
+
+}
