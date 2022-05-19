885bf8b2421a857305f09736489f17096c058fe1
hmk10 <halitmk@gmail.com>
2022-05-19 19:20:58 -0400

    Nested Loop

---
+++ src/Day16/Task_NestedLoop.java
@@ -0,0 +1,27 @@
+package Day16;
+
+public class Task_NestedLoop {
+
+    // Print a square 4X4 wide out of '*' character
+    // ****
+    // ****
+    // ****
+    // ****
+
+    public static void main(String[] args) {
+
+
+        for (int i = 0; i < 4; i++) {
+
+
+            for (int j = 0; j < 4; j++){
+                System.out.print("*");
+            }
+
+            System.out.println();
+
+        }
+
+    }
+
+}
