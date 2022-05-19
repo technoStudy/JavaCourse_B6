ec047a88254ddc0cce59ad059c60595245cc2144
hmk10 <halitmk@gmail.com>
2022-05-19 19:31:26 -0400

    Nested Loop

---
+++ src/Day16/Task2_NestedLoop.java
@@ -0,0 +1,29 @@
+package Day16;
+
+public class Task2_NestedLoop {
+
+    // print following pattern
+    // *****
+    // *****
+    // *****
+    // *****
+    // *****
+    // *****
+
+
+    public static void main(String[] args) {
+
+
+        for (int i = 0; i < 6; i++) {
+
+            for (int j = 0; j < 5; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+
+        }
+
+    }
+
+}
