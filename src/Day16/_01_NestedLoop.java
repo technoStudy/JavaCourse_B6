0a03b2a1a8353f43b45ac1e1d730fa0967890446
hmk10 <halitmk@gmail.com>
2022-05-19 19:14:45 -0400

    Nested Loop

---
+++ src/Day16/_01_NestedLoop.java
@@ -0,0 +1,18 @@
+package Day16;
+
+public class _01_NestedLoop {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("i = " + i);
+
+
+            for (int j = 0; j < 5; j++) {
+                System.out.println("      j = " + j);
+            }
+        }
+
+    }
+
+}
