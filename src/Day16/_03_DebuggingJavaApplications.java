1640709dc967ec4bf972c14365722cdf5cf515cb
hmk10 <halitmk@gmail.com>
2022-05-19 20:43:36 -0400

    Debugging

---
+++ src/Day16/_03_DebuggingJavaApplications.java
@@ -0,0 +1,28 @@
+package Day16;
+
+public class _03_DebuggingJavaApplications {
+
+    public static void main(String[] args) {
+
+        /*
+        Start
+        0
+        1
+        2
+        3
+        4
+        Stop
+         */
+
+
+        System.out.println("Start");
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+        }
+
+        System.out.println("Stop");
+
+    }
+
+}
