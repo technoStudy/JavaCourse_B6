351651334bf1fe47b9d060a22657a1ade96a0f17
hmk10 <halitmk@gmail.com>
2022-05-19 21:45:01 -0400

    JAVA | Array

---
+++ src/Day16/Arrays/_02_JavaArray.java
@@ -0,0 +1,24 @@
+package Day16.Arrays;
+
+public class _02_JavaArray {
+
+    public static void main(String[] args) {
+
+//        [0, 0, 0]
+        int[] myArray = new int[3];
+
+        System.out.println(myArray[0]);
+        System.out.println(myArray[1]);
+        System.out.println(myArray[2]);
+
+        myArray[0] = 5;
+        myArray[1] = 3;
+        myArray[2] = 25;
+
+        System.out.println(myArray[0]);
+        System.out.println(myArray[1]);
+        System.out.println(myArray[2]);
+
+    }
+
+}
