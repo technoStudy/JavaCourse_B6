dae410297dd139e61477d55d6b84f947336e84df
hmk10 <halitmk@gmail.com>
2022-05-20 19:35:30 -0400

    JAVA | Array

---
+++ src/Day17/Task2.java
@@ -0,0 +1,19 @@
+package Day17;
+
+public class Task2 {
+
+    // String[] cities = {"Paris", "New York", "Berlin", "Brooklyn"}
+    // Print all elements in this array (use for loop)
+
+    public static void main(String[] args) {
+
+        String[] cities = {"Paris", "New York", "Berlin", "Brooklyn"};
+
+
+        for (int i = 0; i < cities.length; i++) {
+            System.out.println(cities[i]);
+        }
+
+    }
+
+}
