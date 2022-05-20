2040aa14e32ba5ee293bf75f2b3f9e873735abf5
hmk10 <halitmk@gmail.com>
2022-05-20 19:23:14 -0400

    JAVA | Array

---
+++ src/Day17/_01_PrintingArraysWithLoop.java
@@ -0,0 +1,23 @@
+package Day17;
+
+public class _01_PrintingArraysWithLoop {
+
+    public static void main(String[] args) {
+
+        int[] numbers = {2, 45, 20, 134, 4, 65, 9};
+
+//        System.out.println(numbers[0]);
+//        System.out.println(numbers[1]);
+//        System.out.println(numbers[2]);
+//        System.out.println(numbers[3]);
+//        System.out.println(numbers[4]);
+//        System.out.println(numbers[5]);
+//        System.out.println(numbers[6]);
+
+        for (int i = 0; i <= numbers.length-1; i++) {
+            System.out.println(numbers[i]);
+        }
+
+    }
+
+}
