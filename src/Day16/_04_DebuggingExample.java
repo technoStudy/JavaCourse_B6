c399ad617fda07f1ff9aa15147e6771a4475a00b
hmk10 <halitmk@gmail.com>
2022-05-19 20:52:28 -0400

    Debugging

---
+++ src/Day16/_04_DebuggingExample.java
@@ -0,0 +1,19 @@
+package Day16;
+
+public class _04_DebuggingExample {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 4; i++) {
+
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+
+        }
+
+    }
+
+}
