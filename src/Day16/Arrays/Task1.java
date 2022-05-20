2f7b206d27b6b7b234b930f8c17bb184579a1b8d
hmk10 <halitmk@gmail.com>
2022-05-19 21:51:55 -0400

    JAVA | Array | Task

---
+++ src/Day16/Arrays/Task1.java
@@ -0,0 +1,24 @@
+package Day16.Arrays;
+
+public class Task1 {
+
+    // declare an array of bytes
+    // initialize it with size 3
+    // give each element a random value
+    // print all elements
+
+    public static void main(String[] args) {
+
+        byte[] numbers = new byte[3];
+
+        numbers[0] = 4;
+        numbers[1] = 22;
+        numbers[2] = 1;
+
+        System.out.println(numbers[0]);
+        System.out.println(numbers[1]);
+        System.out.println(numbers[2]);
+
+    }
+
+}
