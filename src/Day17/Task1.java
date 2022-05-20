b56760e15d6cdeb973345c1e80ffad96e6719968
hmk10 <halitmk@gmail.com>
2022-05-20 19:14:02 -0400

    JAVA | Array

---
+++ src/Day17/Task1.java
@@ -0,0 +1,22 @@
+package Day17;
+
+public class Task1 {
+
+    // declare an array of fruits(string) and initialize it with some values | use array = {}
+    // print all elements
+    // print length of your Array
+
+    public static void main(String[] args) {
+
+        String[] fruits = {"Orange", "Mango", "Banana", "Apple"};
+
+        System.out.println(fruits[0]);
+        System.out.println(fruits[1]);
+        System.out.println(fruits[2]);
+        System.out.println(fruits[3]);
+
+        System.out.println("Size of Array ---> " + fruits.length);
+
+    }
+
+}
