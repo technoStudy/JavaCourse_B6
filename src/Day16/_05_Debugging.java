8d9631a1ec818cf936f28f897390e003ff701457
hmk10 <halitmk@gmail.com>
2022-05-19 21:12:01 -0400

    Debugging

---
+++ src/Day16/_05_Debugging.java
@@ -0,0 +1,17 @@
+package Day16;
+
+public class _05_Debugging {
+
+    public static void main(String[] args) {
+
+        String str = "Hello";
+
+        for (int i = 0; i <= str.length(); i++) {
+
+            System.out.println(str.charAt(i));
+
+        }
+
+    }
+
+}
