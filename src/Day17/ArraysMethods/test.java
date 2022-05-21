94fb2c8f4c93bfd65334200c1ae17573e2b071e7
hmk10 <halitmk@gmail.com>
2022-05-20 21:50:41 -0400

    JAVA | Arrays Methods - Sort

---
+++ src/Day17/ArraysMethods/test.java
@@ -0,0 +1,17 @@
+package Day17.ArraysMethods;
+
+import java.util.Arrays;
+
+public class test {
+
+    public static void main(String[] args) {
+
+        String[] test1 = {"1423", "#$%", "jdsjhf", "AGSHGD"};
+
+        Arrays.sort(test1);
+
+        System.out.println(Arrays.toString(test1));
+
+    }
+
+}
