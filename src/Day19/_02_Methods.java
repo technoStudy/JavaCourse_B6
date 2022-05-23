194c2801813f4e7545a7a816e3c1c245059fc8c4
hmk10 <halitmk@gmail.com>
2022-05-23 19:59:41 -0400

    JAVA | Method

---
+++ src/Day19/_02_Methods.java
@@ -0,0 +1,21 @@
+package Day19;
+
+public class _02_Methods {
+
+    public static void main(String[] args) {
+
+        printHello5Times();
+
+    }
+
+
+
+    public static void printHello5Times() {
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+    }
+
+}
