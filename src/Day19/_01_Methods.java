19a1f3d3857fd207774cce4c5b6bec37665ea27e
hmk10 <halitmk@gmail.com>
2022-05-23 19:49:18 -0400

    JAVA | Method

---
+++ src/Day19/_01_Methods.java
@@ -0,0 +1,16 @@
+package Day19;
+
+public class _01_Methods {
+
+    public static void main(String[] args) {
+
+        printHelloWorld(); // calling a method
+
+    }
+
+    // Creating a method - with no return type
+    public static void printHelloWorld() {
+        System.out.println("Hello World!");
+    }
+
+}
