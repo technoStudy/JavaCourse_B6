a77b1d75caa67bc68a4620f2990b8afc9c45533c
hmk10 <halitmk@gmail.com>
2022-05-23 21:15:01 -0400

    JAVA | Methods with Return Type

---
+++ src/Day19/_03_MethodReturnTypes.java
@@ -0,0 +1,16 @@
+package Day19;
+
+public class _03_MethodReturnTypes {
+
+    public static void main(String[] args) {
+        String str = greet();
+        System.out.println(str);
+    }
+
+
+    public static String greet() {
+        String greetMessage = "Hello";
+        return greetMessage;
+    }
+
+}
