a04934697075cacde74046e6557d578a509f35aa
hmk10 <halitmk@gmail.com>
2022-05-23 20:26:51 -0400

    JAVA | Method Task

---
+++ src/Day19/Task1_Methods.java
@@ -0,0 +1,18 @@
+package Day19;
+
+public class Task1_Methods {
+
+    //                  TASK
+    // Create a function which prints numbers from 0 to 10
+
+    public static void main(String[] args) {
+        printNumbers0To10();
+    }
+
+
+    public static void printNumbers0To10() {
+        for (int i = 0; i <=10; i++)
+            System.out.println(i);
+    }
+
+}
