fb54f60c2956a4364550ab43c7734755dee908a1
hmk10 <halitmk@gmail.com>
2022-05-23 20:42:10 -0400

    JAVA | Methods with Parameters

---
+++ src/Day19/_02_MethodsParameters2.java
@@ -0,0 +1,14 @@
+package Day19;
+
+public class _02_MethodsParameters2 {
+
+    public static void main(String[] args) {
+        greeting("LeBron", "James");
+    }
+
+
+    public static void greeting(String firstName, String lastName) {
+        System.out.println("Hello " + firstName + " " + lastName);
+    }
+
+}
