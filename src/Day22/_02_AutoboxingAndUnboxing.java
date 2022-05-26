6b0bdae4119784645bb58e2da489e2f337316a30
hmk10 <halitmk@gmail.com>
2022-05-26 19:36:08 -0400

    JAVA | Wrapper Classes - Autoboxing and Unboxing

---
+++ src/Day22/_02_AutoboxingAndUnboxing.java
@@ -0,0 +1,15 @@
+package Day22;
+
+public class _02_AutoboxingAndUnboxing {
+
+    public static void main(String[] args) {
+
+        byte primitiveVariable = 10;
+        Byte referenceVariable =  primitiveVariable; // autoboxing
+
+        Character referenceChar = 'A';
+        char primitiveChar = referenceChar; // unboxing
+
+    }
+
+}
