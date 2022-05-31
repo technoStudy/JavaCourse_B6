e12c25bd30317f9238cdbbca2e5c5117fa755516
hmk10 <halitmk@gmail.com>
2022-05-31 19:25:30 -0400

    JAVA | Set

---
+++ src/Day24/_01_SetIntro.java
@@ -0,0 +1,18 @@
+package Day24;
+
+public class _01_SetIntro {
+
+    /**
+
+        SET
+            - It's unordered collection of objects!
+            - Set cannot store duplicate objects!
+
+
+        ArrayList VS Set
+            - List can have duplicate elements but Set cannot have duplicate elements!
+            - In List elements are stored in insertion order, Elements in Set stored unordered!
+
+     */
+
+}
