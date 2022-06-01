77a51ae40856ef1c9a143e4667ae7d62ce751b33
hmk10 <halitmk@gmail.com>
2022-05-31 21:44:33 -0400

    JAVA | Set

---
+++ src/Day24/HOMEWORK.java
@@ -0,0 +1,10 @@
+package Day24;
+
+public class HOMEWORK {
+
+    // Homework
+    // create an arrayList of numbers 1, 3, 1, 2, 4, 5, 3 --> 2 duplicates
+    // count how many duplicates we have
+    // find out what are the duplicated values
+
+}
