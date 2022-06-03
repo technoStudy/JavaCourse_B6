69a5aa18115cfa4a5f6abf7e1daff7d1c0b5fc1c
hmk10 <halitmk@gmail.com>
2022-06-03 19:46:37 -0400

    JAVA | Collections Recap

---
+++ src/Day26/_00_CollectionsRecap.java
@@ -0,0 +1,32 @@
+package Day26;
+
+public class _00_CollectionsRecap {
+
+    // ARRAY VS ARRAYLIST
+    // Array is fixed-size, ArrayList is resizable
+    // Array can store both primitive and reference type, ArrayList can only store Reference
+    // length() method used to calculate size of Array, size() method used for ArrayList
+    // ArrayList is part of Collections, Array is not!
+
+    // Both Array and ArrayList can store duplicate values!!
+
+
+    // ARRAYLIST VS SET
+    // Set is unordered, ArrayList uses insertion order(indexes)!
+    // Set doesn't allow duplicate values, ArrayList allows duplicate values!
+
+
+    // HashSet
+    // Unordered Set - doesn't allow duplicate values
+
+    // LinkedHashSet
+    // It is using insertion order - doesn't allow duplicate values
+
+    // TreeSet
+    // It is an ordered Set (using Natural Order - Ascending Order) - doesn't allow duplicate values
+
+
+
+    // ****  We don't have get() method for Sets - because Sets don't have indexes!!!!
+
+}
