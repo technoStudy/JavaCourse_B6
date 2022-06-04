a747c0ee204fe5cbd1be5197d2e22b9a31015eee
hmk10 <halitmk@gmail.com>
2022-06-03 20:40:58 -0400

    JAVA | 2D Collections

---
+++ src/Day26/Task_2DCollections.java
@@ -0,0 +1,33 @@
+package Day26;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Task_2DCollections {
+
+    // TASK
+    // Create 2D ArrayList which can store String ArrayLists
+    // Create 3 ArrayLists which are Employees, Employers, Companies
+    // Store this 3 ArrayList in 2D ArrayList
+
+    public static void main(String[] args) {
+
+        ArrayList<ArrayList<String>> listOfLists = new ArrayList<>();
+
+        ArrayList<String> employees = new ArrayList<>();
+        Collections.addAll(employees, "Mike", "John", "Emily");
+
+        ArrayList<String> employers = new ArrayList<>();
+        Collections.addAll(employers, "Elon Musk", "Bill Gates", "Jeff Bezos");
+
+        ArrayList<String> companies = new ArrayList<>();
+        Collections.addAll(companies, "Tesla", "Microsoft", "Amazon");
+
+
+        Collections.addAll(listOfLists, employees, employers, companies);
+
+        System.out.println(listOfLists);
+
+    }
+
+}
