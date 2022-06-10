14f867bc330e3a3f2f11d59655f4272e2da65a22
hmk10 <halitmk@gmail.com>
2022-06-10 19:33:08 -0400

    JAVA | OOP - Static Methods

---
+++ src/Day31/_01_Main.java
@@ -0,0 +1,15 @@
+package Day31;
+
+public class _01_Main {
+
+    public static void main(String[] args) {
+
+        int[] numbers = {1, 4, 2, 9, 4, 7, 8, 0, 3};
+        _01_UsefulMethods.printArray(numbers);
+
+        int maxValue = Math.max(5, 3);
+        double randomValue = Math.random();
+
+    }
+
+}
---
+++ src/Day31/_01_UsefulMethods.java
@@ -0,0 +1,10 @@
+package Day31;
+
+public class _01_UsefulMethods {
+
+    public static void printArray(int[] array) {
+        for (int i = 0; i < array.length; i++)
+            System.out.println(array[i]);
+    }
+
+}
