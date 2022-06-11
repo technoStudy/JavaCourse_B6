d13156275d77192f3fbada9221c5093bea5f64fe
hmk10 <halitmk@gmail.com>
2022-06-10 21:47:24 -0400

    JAVA | OOP - Constructor

---
+++ src/Day31/ConstructorTask2/Main.java
@@ -0,0 +1,13 @@
+package Day31.ConstructorTask2;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Rectangle rectangle = new Rectangle(5, 7);
+
+        System.out.println(rectangle.area());
+
+    }
+
+}
