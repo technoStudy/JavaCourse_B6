5e4f47618475e1ffaa4810fa198e310053eaa126
hmk10 <halitmk@gmail.com>
2022-06-10 21:34:01 -0400

    JAVA | OOP - Constructor

---
+++ src/Day31/ConstructorTask1/Circle.java
@@ -0,0 +1,17 @@
+package Day31.ConstructorTask1;
+
+public class Circle {
+
+    int radius;
+
+    // No-Args constructor
+    public Circle() {
+        radius = 10;
+    }
+
+    // Parametrized Constructor
+    public Circle(int radius) {
+        this.radius = radius;
+    }
+
+}
---
+++ src/Day31/ConstructorTask1/Main.java
@@ -0,0 +1,18 @@
+package Day31.ConstructorTask1;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Circle circle = new Circle();
+
+        System.out.println(circle.radius);
+
+
+        Circle circle2 = new Circle(20);
+
+        System.out.println(circle2.radius);
+
+    }
+
+}
