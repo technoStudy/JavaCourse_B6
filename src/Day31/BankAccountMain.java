2120d2fc9aa694d0728676a32e4cbaa77e3c9514
hmk10 <halitmk@gmail.com>
2022-06-10 20:41:47 -0400

    JAVA | OOP - Static Methods

---
+++ src/Day31/BankAccountMain.java
@@ -0,0 +1,16 @@
+package Day31;
+
+public class BankAccountMain {
+
+    public static void main(String[] args) {
+
+        BankAccount bankAccount = new BankAccount("Mark", 123456789);
+
+        bankAccount.deposit(1000);
+
+        System.out.println(bankAccount.customerName);
+        System.out.println(bankAccount.customerSSN);
+
+    }
+
+}
