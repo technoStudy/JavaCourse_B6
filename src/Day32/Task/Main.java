c3d41c904c3e4fe99b99f4f502e9b6a21bab2e7a
hmk10 <halitmk@gmail.com>
2022-06-13 20:21:01 -0400

    JAVA | OOP - Access Modifiers for Attributes, Methods and Constructors

---
+++ src/Day32/Task/Main.java
@@ -0,0 +1,18 @@
+package Day32.Task;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+//        Cannot access constructor - PRIVATE
+//        Student student1 = new Student();
+
+        Student student2 = new Student("Koray", 25);
+
+//        Cannot access fields - PRIVATE
+//        System.out.println(student2.name);
+//        System.out.println(student2.age);
+
+    }
+
+}
---
+++ src/Day32/Task/Student.java
@@ -0,0 +1,16 @@
+package Day32.Task;
+
+public class Student {
+
+    private String name;
+    private int age;
+
+    // Default Constructor
+    private Student() {}
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+}
