5cae0d9438bd1aa15d326a3778f4d008bf3867dd
hmk10 <halitmk@gmail.com>
2022-06-14 21:31:58 -0400

    JAVA | OOP - Encapsulation

---
+++ src/Day34/Task/Student.java
@@ -0,0 +1,42 @@
+package Day34.Task;
+
+public class Student {
+
+    private String name;
+    private int age;
+    private String country;
+    private int grade;
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+
+}
