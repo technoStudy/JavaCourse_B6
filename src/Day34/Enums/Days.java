3b2ae507cf5b5bfde9a4a6a31a2c0fda1dc8eef2
hmk10 <halitmk@gmail.com>
2022-06-14 21:41:36 -0400

    JAVA | OOP - Enums

---
+++ src/Day34/Enums/Days.java
@@ -0,0 +1,17 @@
+package Day34.Enums;
+
+public enum Days {
+
+    /**
+        To store multiple constants we use ENUMS
+     */
+
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+    SUNDAY
+
+}
---
+++ src/Day34/Enums/Main.java
@@ -0,0 +1,14 @@
+package Day34.Enums;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(Days.TUESDAY);
+
+        Days lastDayOfTheWeek = Days.SUNDAY;
+        System.out.println(lastDayOfTheWeek);
+
+    }
+
+}
