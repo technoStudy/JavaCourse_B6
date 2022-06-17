3851aef444093e7665953284f6285f75668d8dcf
hmk10 <halitmk@gmail.com>
2022-06-16 21:15:39 -0400

    JAVA | OOP - Inheritance

---
+++ src/Day35/Cat.java
@@ -0,0 +1,13 @@
+package Day35;
+
+public class Cat extends Animal {
+
+    public Cat(String name, int age) {
+        super(name, age);
+    }
+
+    public void catchFly() {
+        System.out.println("Cat is catching fly!");
+    }
+
+}
