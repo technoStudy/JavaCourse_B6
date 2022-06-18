0ad9691b044885d943ecb2c40c6336e2842d823f
hmk10 <halitmk@gmail.com>
2022-06-18 12:09:22 -0400

    JAVA | OOP - Method Overriding

--- src/Day37/Task/Main.java
+++ src/Day37/Task/Main.java
@@ -5,6 +5,9 @@ public class Main {
     public static void main(String[] args) {
 
         Baby baby = new Baby("Emily", "Female", 1);
+
+        System.out.println(baby.name);
+
         baby.eat();
         baby.transport();
 
