bf8fff787cc31ab57d20ddf89332df641a907a76
hmk10 <halitmk@gmail.com>
2022-06-28 21:51:15 -0400

    JAVA | Exceptions - try catch finally

---
+++ src/Day42/TryCatchFinally.java
@@ -0,0 +1,17 @@
+package Day42;
+
+public class TryCatchFinally {
+
+    public static void main(String[] args) {
+
+        try {
+            // try to do this
+        } catch (Exception ex) {
+            // if there is an exception do this
+        } finally {
+            // do this anyway
+        }
+
+    }
+
+}
