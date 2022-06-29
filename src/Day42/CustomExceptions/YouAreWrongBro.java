10b9ff65329b5ba7d18f369288722f61cee9251e
hmk10 <halitmk@gmail.com>
2022-06-28 22:04:49 -0400

    JAVA | Exceptions - Custom Exceptions

--- src/Day42/CustomExceptions/YouAreWrongBro.java
+++ src/Day42/CustomExceptions/YouAreWrongBro.java
@@ -2,4 +2,10 @@ package Day42.CustomExceptions;
 
 public class YouAreWrongBro extends RuntimeException {
 
+    /**
+     * If you want to create checked exception you need to extend "Exception" class
+     *
+     * If you want to create unchecked exception you need to extend "RuntimeException" class
+     */
+
 }
